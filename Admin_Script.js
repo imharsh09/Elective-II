773a66371f8b027a24676bd26da7c5503d326e70
imharsh09 <102022721+imharsh09@users.noreply.github.com>
2022-03-21 20:28:28 +0530

    Add files via upload

---
+++ Admin_Script.js
@@ -0,0 +1,16 @@
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".sidebarBtn");
+sidebarBtn.onclick = function() {
+  sidebar.classList.toggle("active");
+  if(sidebar.classList.contains("active")){
+  sidebarBtn.classList.replace("bx-menu" ,"bx-menu-alt-right");
+}else
+  sidebarBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+}
+
+
+let redirection = document.getElementsByClassName(".Dashboard-redirecting");
+
+redirection.onclick = function(){
+  window.location.reload(true);
+}
